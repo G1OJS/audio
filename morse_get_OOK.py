cea34583a96f46d32db422ac286dd5295efb19e3
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-02-01 20:48:25 +0000

    rename

--- morse_get_OOK.py
+++ morse_get_OOK.py
@@ -6,13 +6,13 @@ import time
 import threading
 import matplotlib.pyplot as plt
     
-global waterfall, key, wpm, ticker
+global waterfall
 
 class AudioFrontEnd:
     
     def __init__(self, device_keywords = ['Mic', 'CODEC']):
         global waterfall
-        waterfall = {'dur':1, 'dt':0.0005, 'df':50, 'fMax':1000, 'nf':0, 'waterfall': None}
+        waterfall = {'dur':1, 'dt':0.0005, 'df':50, 'fMax':2000, 'nf':0, 'waterfall': None}
         waterfall.update({'nF': int(waterfall['fMax'] / waterfall['df'])})
         waterfall.update({'waterfall': np.zeros((waterfall['nF'], int(waterfall['dur'] / waterfall['dt'])))})             
         self.pya = pyaudio.PyAudio()
@@ -63,30 +63,30 @@ class AudioFrontEnd:
            
 class TimingDecoder:
 
-    def __init__(self):
-        global key
-        key = np.zeros(waterfall['waterfall'].shape[1])
+    def __init__(self, ax):
+        self.ax = ax
+        self.key_is_down = False
+        self.n_fbins = waterfall['waterfall'].shape[0]
+        self.fbin = 0
         self.dot = 0.1
         self.max_dot = 0.15
         self.min_dot = 0.03
+        self.ticker = False
+        self.set_fbin(10)
         self.symbols = ""
         threading.Thread(target = self.get_symbols).start()
         threading.Thread(target = self.decoder).start()
 
-    def get_symbols(self):
-        global key
-        
-        def hysteresis(signal, lo=0.3, hi=0.6):
-            out = np.zeros_like(signal, dtype=bool)
-            state = False
-            for i, v in enumerate(signal):
-                if not state and v > hi:
-                    state = True
-                elif state and v < lo:
-                    state = False
-                out[i] = state
-            return out
+    def set_fbin(self, fbin):
+        if(fbin == self.fbin):
+            return
+        if(self.ticker):
+            self.ticker.set_text(" " * 20)
+        self.fbin = fbin
+        self.ticker = self.ax.text(0, self.fbin / self.n_fbins,'')
+        self.ticker_text = []
 
+    def get_symbols(self):
         t_key_down = False
         t_key_up = time.time()
         s = ""
@@ -95,10 +95,14 @@ class TimingDecoder:
         
         while(True):
             time.sleep(0.001)
-            key = hysteresis(waterfall['waterfall'][int(waterfall['waterfall'].shape[0]/2), :])
+            
+            level = waterfall['waterfall'][self.fbin, -1]
+            if not self.key_is_down and level > 0.6:
+                self.key_is_down = True
+            elif self.key_is_down and level < 0.3:
+                self.key_is_down = False
 
-            key_is_down = key[-1]
-            if(not key_is_down and t_key_down):
+            if(not self.key_is_down and t_key_down):
                 t_key_up = time.time()
                 down_duration = t_key_up - t_key_down
                 t_key_down = False
@@ -113,12 +117,11 @@ class TimingDecoder:
                 if(time.time() - t_key_up > 1.5*self.dot and len(s)):
                     self.symbols = s
                     s = ""
-            if(key_is_down and t_key_up):
+            if(self.key_is_down and t_key_up):
                 t_key_down = time.time()
                 t_key_up = False
 
     def decoder(self):
-        global wpm, ticker
         MORSE = {
         ".-": "A",    "-...": "B",  "-.-.": "C",  "-..": "D",
         ".": "E",     "..-.": "F",  "--.": "G",   "....": "H",
@@ -133,40 +136,42 @@ class TimingDecoder:
         "---..": "8", "----.": "9"
         }
 
-        ticker_text = []
         last_symbols = time.time()
         while(True):
             time.sleep(0.05)
             if(len(self.symbols)):
                 last_symbols = time.time()
-                wpm.set_text(f"{int(60 / (50 * self.dot))} wpm")
-                ticker_text.append(MORSE.get(self.symbols, "?"))
-                ticker_text = ticker_text[-20:]
-                ticker.set_text(''.join(ticker_text))
+                self.ticker_text.append(MORSE.get(self.symbols, "?"))
+                self.ticker_text = self.ticker_text[-20:]
+                self.ticker.set_text(f"{int(60 / (50 * self.dot))} {''.join(self.ticker_text)}")
                 self.symbols = ""
-            if(time.time() - last_symbols > 14*self.dot and len(ticker_text)):
-                if(ticker_text[-1] != " "):
-                    ticker_text.append(" ")
-                    ticker.set_text(''.join(ticker_text))
+            if(time.time() - last_symbols > 14*self.dot and len(self.ticker_text)):
+                if(self.ticker_text[-1] != " "):
+                    self.ticker_text.append(" ")
 
+        
 def run():
-    global waterfall, key, wpm, ticker
-    fig, axs = plt.subplots(2,1, figsize = (8,3))
+    global waterfall
+    fig, axs = plt.subplots(1,2, figsize = (8,8))
     audio = AudioFrontEnd()
-    waterfall_plot = axs[0].imshow(waterfall['waterfall'], extent = (0, 5000, 0, 2000))
-    
-    decoder = TimingDecoder()
-    key_plot, = axs[1].plot(key)
-    axs[1].set_ylim(0,2)
-    wpm = fig.text(0.1,0.6,"WPM")
-    ticker = fig.text(0.1,0.8,"TEXT")
+    waterfall_plot = axs[0].imshow(waterfall['waterfall'], origin = 'lower', aspect='auto')
+    axs[0].set_xticks([])
+    axs[0].set_yticks([])
+    axs[1].set_axis_off()
 
+    n_decoders = 2
+    decoders = []
+    for i in range(n_decoders):
+        decoders.append(TimingDecoder(axs[1]))
 
     while True:
         time.sleep(0.01)
-        waterfall_plot.set_data(waterfall['waterfall'].copy())
+        waterfall_plot.set_data(waterfall['waterfall'])
         waterfall_plot.autoscale()
-        key_plot.set_ydata(key)
+        bin_powers = np.mean(waterfall['waterfall'], axis = 1)
+        top_bins = np.argsort(bin_powers)[:n_decoders:-1]
+        for i, decoder in enumerate(decoders):
+            decoder.set_fbin(top_bins[i])
         plt.pause(0.1)
 
 run()
