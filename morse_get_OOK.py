b3a69969b50685ed56d5746225d10d83061c622d
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-02-17 20:25:13 +0000

    working MVP-style

--- morse_get_OOK.py
+++ morse_get_OOK.py
@@ -8,13 +8,13 @@ from audio import Audio_in
 F_RANGE = [200, 1500]
 NDECODERS = 3
 SHOW_KEYLINES = True
-SPEED = {'MAX':45, 'MIN':12, 'ALPHA':0.1}
+SPEED = {'MAX':45, 'MIN':12, 'ALPHA':0.05}
 TICKER_FIELD_LENGTHS = {'MORSE':30, 'TEXT':30}
 TIMESPEC = {'DOT_SHORT':0.65, 'DOT_LONG':2, 'CHARSEP_SHORT':1.5, 'CHARSEP_LONG':4, 'WORDSEP':6.5}
-AUDIO_DT = 0.02
+AUDIO_REFRESH_DT = 0.02
 AUDIO_RES= 80
-DISPLAY_DT = -1
-DISPLAY_DUR = 1
+DISPLAY_REFRESH_DT = .08
+DISPLAY_DUR = 2
 MORSE = {
 ".-": "A",    "-...": "B",  "-.-.": "C",  "-..": "D",
 ".": "E",     "..-.": "F",  "--.": "G",   "....": "H",
@@ -105,15 +105,13 @@ class TimingDecoder:
         else:
             self.morse_elements = self.morse_elements + el
                 
-    def step(self, sigs):
-        if(self.fbin>-1):
-            sig = sigs[self.fbin]
-            mark_dur, space_dur, is_idle = self.detect_transition(sig)
-            if any([mark_dur, space_dur, is_idle]):
-                el = self.classify_duration(mark_dur, space_dur, is_idle)
-                if(mark_dur):
-                    self.update_speed(mark_dur)
-                self.process_element(el)
+    def step(self, sig):
+        mark_dur, space_dur, is_idle = self.detect_transition(sig)
+        if any([mark_dur, space_dur, is_idle]):
+            el = self.classify_duration(mark_dur, space_dur, is_idle)
+            if(mark_dur):
+                self.update_speed(mark_dur)
+            self.process_element(el)
 
 class UI_decoder:
     def __init__(self, axs, fbin, timevals):
@@ -122,50 +120,55 @@ class UI_decoder:
         self.decoder = TimingDecoder(fbin)
         self.ticker = None
         self.keyline = None
+        self.s_meter = 0
         self.set_fbin(fbin)
 
     def set_fbin(self, fbin):
+        self.fbin = fbin
         if(self.ticker is not None):
             self.ticker.set_text(' ' * len(self.decoder.ticker_dict['rendered_text']))
             self.ticker.remove()
         if(self.keyline is not None):
             self.keyline['line'].remove()
-        self.fbin = fbin
         self.decoder.set_fbin(fbin)
         self.ticker = self.axs[1].text(-0.15, fbin, '*')
         kld = np.zeros_like(self.timevals)
-        self.keyline = {'data':kld, 'line':self.axs[0].plot(self.timevals, kld, color = 'black')[0]}
-
-class Audio_processor:
-    def __init__(self, audio):
-        self.audio = audio
-        nf = audio.params['nf']
-        self.dt = audio.params['dt']
-        self.noise = np.ones(nf)
-        self.noise_decaying = np.zeros(nf)
-        self.decaying_max = np.ones(nf)
-        self.s_meter = np.zeros(nf)
-        self.sig = np.zeros(nf)
-        threading.Thread(target = self.run).start()
-
-    def run(self):
-        while True:
-            time.sleep(self.dt)
-            self.noise = np.minimum(self.noise, self.audio.pwr)
-            self.noise_decaying = np.maximum(self.noise_decaying*0.95, self.noise)
-            sig_unnorm = self.audio.pwr / self.noise_decaying
-            self.s_meter = np.maximum(self.s_meter * 0.9, sig_unnorm)
-            self.decaying_max = np.maximum(self.decaying_max*0.95, sig_unnorm)
-            self.sig = sig_unnorm / self.decaying_max
+        self.keyline = {'data':kld, 'line':self.axs[0].plot(self.timevals, kld, color = 'white', drawstyle='steps-post')[0]}
 
 class App:
     def __init__(self):
-        self.audio = Audio_in(df = AUDIO_RES, dt = AUDIO_DT,  fRng = F_RANGE)
-        self.display_dt = DISPLAY_DT if DISPLAY_DT > 0 else self.audio.params['dt']
-        self.dsp = Audio_processor(self.audio)
-        self.display_nt = int(DISPLAY_DUR / self.display_dt)
+        self.audio = Audio_in(df = AUDIO_RES, dt = AUDIO_REFRESH_DT,  fRng = F_RANGE)
+        self.display_refresh_dt = DISPLAY_REFRESH_DT if DISPLAY_REFRESH_DT > 0 else self.audio.params['dt']
+        self.display_nt = int(DISPLAY_DUR / self.audio.params['dt'])
+        self.waterfall = np.zeros((self.audio.params['nf'], self.display_nt))
+        self.decoders = []
+        self.s_meter = np.zeros(self.audio.params['nf'])
         self.timevals = np.linspace(0, DISPLAY_DUR, self.display_nt)
+        threading.Thread(target = self.dsp).start()
         self.animate()
+
+    def dsp(self):
+        nf = self.audio.params['nf']
+        dt = self.audio.params['dt']
+        noise = np.ones(nf)
+        noise_decaying = np.zeros(nf)
+        self.sig_unnorm = np.ones(nf)
+        self.sig = np.ones(nf)
+        decaying_max = np.ones(nf)
+        
+        while True:
+            time.sleep(dt)
+            noise = np.minimum(noise, self.audio.pwr)
+            noise_decaying = np.maximum(noise_decaying*0.9, noise)
+            self.sig_unnorm = self.audio.pwr / noise_decaying
+            decaying_max = np.maximum(decaying_max*0.95, self.sig_unnorm)
+            self.sig = self.sig_unnorm / decaying_max
+            for d in self.decoders:
+                d.decoder.step(self.sig[d.decoder.fbin])
+                d.keyline['data'][-1] = 0.2 + 0.6 * d.decoder.keypos + d.fbin
+                d.keyline['data'][:-1] = d.keyline['data'][1:]
+            self.waterfall = np.roll(self.waterfall, -1, axis =1)
+            self.waterfall[:, -1]  = self.sig_unnorm
 
     def animate(self):
         fig, axs = plt.subplots(1,2, figsize = (14,2))
@@ -175,48 +178,43 @@ class App:
         axs[1].set_axis_off()
         self.decoders = [UI_decoder(axs, fb, self.timevals) for fb in range(NDECODERS)]
         
-        waterfall = np.zeros((self.audio.params['nf'], self.display_nt))
-        spec_plot = axs[0].imshow(waterfall, origin = 'lower', aspect='auto', alpha = 1, 
+        spec_plot = axs[0].imshow(self.waterfall, origin = 'lower', aspect='auto', alpha = 1, 
                                       interpolation = 'none', extent=[0, DISPLAY_DUR, 0, self.audio.params['nf']])
         def refresh(i):
-            nonlocal waterfall, spec_plot, axs
+            nonlocal spec_plot, axs
+            snr_db = 10 * np.log10(self.sig_unnorm)
+            self.s_meter = np.maximum(self.s_meter * 0.999, snr_db)
+            
             if(i % 100 == 0):
-                fbins_to_decode = np.argsort(-self.dsp.s_meter)[:NDECODERS]
-                decoders_sorted = sorted(self.decoders, key=lambda d: self.dsp.s_meter[d.fbin])
+                fbins_to_decode = np.argsort(-self.s_meter)[:NDECODERS]
+                decoders_sorted = sorted(self.decoders, key=lambda d: self.s_meter[d.fbin])
                 current_bins_with_decoders = [d.fbin for d in self.decoders]
                 for fb in fbins_to_decode:
                     if fb not in current_bins_with_decoders:
                         weakest_decoder = decoders_sorted[0]
                         weakest_decoder.set_fbin(fb)
                         break
-                
-            for decoder in self.decoders:
-                decoder.s_meter = self.dsp.s_meter[decoder.fbin]
-                decoder.decoder.step(self.dsp.sig)
 
-            waterfall = np.roll(waterfall, -1, axis =1)
-            waterfall[:, -1]  = self.dsp.sig
-            spec_plot.set_array(waterfall)
+            spec_plot.set_array(self.waterfall)
             spec_plot.autoscale()
             
             if(SHOW_KEYLINES):
                 for d in self.decoders:
-                    d.keyline['data'][-1] = 0.2 + 0.6 * d.decoder.keypos + d.decoder.fbin
-                    d.keyline['data'][:-1] = d.keyline['data'][1:]
+
                     d.keyline['line'].set_ydata(d.keyline['data'])
 
             for d in self.decoders:
                 if(d is not None):
                     td = d.decoder.ticker_dict
-                    text = f"{np.log(d.s_meter):+02.0f} {td['wpm']:4.1f}   {td['morse']}  {td['text'].strip()}"
+                    s = self.s_meter[d.fbin]
+                    text = f"{s - 80:+03.0f}dB {td['wpm']:3.0f}wpm   {td['morse']}  {td['text'].strip()}"
                     if(td['rendered_text'] != text):
                         d.ticker.set_text(text) 
                         td['rendered_text'] = text
- 
 
             return None
         
-        ani = FuncAnimation(plt.gcf(), refresh, interval = self.display_dt * 1000, frames=range(100000), blit=False)
+        ani = FuncAnimation(plt.gcf(), refresh, interval = self.display_refresh_dt * 1000, frames=range(100000), blit=False)
         plt.show()
         
 app = App()
